e37eb394e465e6b90bd137fad846dcaaf518d715
reijjo <teemu.aitomeri@gmail.com>
2024-01-07 22:36:32 +0200

    colors selected

---
+++ client/src/pages/Landing.tsx
@@ -0,0 +1,38 @@
+const Landing = () => {
+  return (
+    <div className="testing">
+      <h1 style={{ color: "var(--success)" }}>hihoo success VARI??</h1>
+      <h1 style={{ color: "var(--successmid)" }}>hihoo successmid VARI??</h1>
+      <h1 style={{ color: "var(--successlight)" }}>
+        hihoo successlight VARI??
+      </h1>
+      <h1 style={{ color: "var(--warn)" }}>hihoo warn VARI??</h1>
+      <h1 style={{ color: "var(--warnmid)" }}>hihoo warnmid VARI??</h1>
+      <h1 style={{ color: "var(--warnlight)" }}>hihoo warnlight VARI??</h1>
+      <h1 style={{ color: "var(--error)" }}>hihoo error VARI??</h1>
+      <h1 style={{ color: "var(--errormid)" }}>hihoo errormid VARI??</h1>
+      <h1 style={{ color: "var(--errorlight)" }}>hihoo errorlight VARI??</h1>
+      <h1 style={{ color: "var(--white)" }}>hihoo white VARI??</h1>
+      <h1 style={{ color: "var(--black)" }}>hihoo black VARI??</h1>
+
+      <h1 style={{ color: "var(--primary" }}>hihoo primary VARI??</h1>
+      <h1 style={{ color: "var(--primarydark" }}>hihoo primarydark VARI??</h1>
+      <h1 style={{ color: "var(--primarymid)" }}>hihoo primarymid VARI??</h1>
+      <h1 style={{ color: "var(--primarylight)" }}>
+        hihoo primarylight VARI??
+      </h1>
+
+      <h1 style={{ color: "var(--secondary)" }}>hihoo secondary VARI??</h1>
+      <h1 style={{ color: "var(--secondarymid)" }}>
+        hihoo secondarymid VARI??
+      </h1>
+      <h1 style={{ color: "var(--secondarylight)" }}>
+        hihoo secondarydark VARI??
+      </h1>
+      <h1 style={{ color: "var(--bg)" }}>hihoo bg VARI??</h1>
+      <h1 style={{ color: "var(--text)" }}>hihoo text VARI??</h1>
+    </div>
+  );
+};
+
+export default Landing;
